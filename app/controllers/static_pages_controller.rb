e8af498c1403703cca7bc843e831cf270b492a32
Brian <Brian@Brians-MacBook-Pro.local>
2016-09-19 21:56:05 +0100

    first commit of clone from tutorial app

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,15 @@
+class StaticPagesController < ApplicationController
+  def home
+  	#render text: "This is Home"
+  end
+
+  def help
+  	#render text: "This is help"
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
